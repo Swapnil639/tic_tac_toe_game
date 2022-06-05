ad9ce91b9829cacf22be9543d74fc83a9114a7f7
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-06 01:30:07 +0530

    Added: Created Game Board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -3,28 +3,41 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-    public static void main(String[] args){
+    static char[] board = new char[10];
+
+    public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe game !");
         createBoard();
         playerChoice();
+        showBoard();
     }
-    public static void createBoard(){
-        char[] board = new char[10];
-        for (int i=0;i<board.length;i++){
-            board[i]=' ';
+
+    public static void createBoard() {
+
+        for (int i = 0; i < board.length; i++) {
+            board[i] = ' ';
         }
     }
-    public static void playerChoice(){
+
+    public static void playerChoice() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Choose x or o : ");
         char player = scanner.next().charAt(0);
         char computerPlayer;
 
-        if (player == 'x'){
+        if (player == 'x') {
             computerPlayer = 'o';
-        }else{
+        } else {
             computerPlayer = 'x';
         }
-        System.out.println("player choice is: "+player+" computer choice is: "+ computerPlayer);
+        System.out.println("player choice is: " + player + " computer choice is: " + computerPlayer);
+    }
+
+    public static void showBoard() {
+        System.out.println(" " + board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println("-----------");
+        System.out.println(" " + board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println("-----------");
+        System.out.println(" " + board[7] + " | " + board[8] + " | " + board[9]);
     }
 }
