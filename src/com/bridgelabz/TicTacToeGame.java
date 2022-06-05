a0487a0cdac52eb7b310a5543247d659c09c165e
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-06 00:32:48 +0530

    Added :Creating a tic tac toe board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,9 +1,14 @@
 package com.bridgelabz;
 
-public class TicTacToeGame
-{
-    public static void main(String[] args)
-    {
+public class TicTacToeGame {
+    public static void main(String[] args){
         System.out.println("Welcome to Tic Tac Toe game !");
+        createBoard();
+    }
+    public static void createBoard(){
+        char[] board = new char[10];
+        for (int i=0;i<board.length;i++){
+            board[i]=' ';
+        }
     }
 }
