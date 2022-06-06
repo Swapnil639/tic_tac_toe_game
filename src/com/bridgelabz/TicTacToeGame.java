19b7eef30767da699415c16fe0d2f53696a601fc
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-06 21:50:51 +0530

    Added :User can move in the board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
     static char[] board = new char[10];
+    static char player;
+    static char computerPlayer;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe game !");
@@ -12,27 +14,30 @@ public class TicTacToeGame {
         showBoard();
     }
 
+    // UC1 create board
     public static void createBoard() {
-
-        for (int i = 0; i < board.length; i++) {
+        for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
     }
 
+    // UC2 player choose letter x or o
     public static void playerChoice() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Choose x or o : ");
-        char player = scanner.next().charAt(0);
-        char computerPlayer;
-
-        if (player == 'x') {
-            computerPlayer = 'o';
+        System.out.print("Choose X or O : ");
+        player = scanner.next().toUpperCase().charAt(0);
+        if (player == 'X') {
+            computerPlayer = 'O';
         } else {
-            computerPlayer = 'x';
+            computerPlayer = 'X';
         }
-        System.out.println("player choice is: " + player + " computer choice is: " + computerPlayer);
+        while (true) {
+            makeMove();
+            showBoard();
+        }
     }
 
+    // UC3 display board
     public static void showBoard() {
         System.out.println(" " + board[1] + " | " + board[2] + " | " + board[3]);
         System.out.println("-----------");
@@ -40,4 +45,18 @@ public class TicTacToeGame {
         System.out.println("-----------");
         System.out.println(" " + board[7] + " | " + board[8] + " | " + board[9]);
     }
+
+    // UC4  make move to a desired location
+    public static void makeMove() {
+        System.out.println("choose an index from 1 to 9 to write x or o :");
+        Scanner scanner = new Scanner(System.in);
+        int index = scanner.nextInt();
+        if (board[index] == ' ') {
+            board[index] = player;
+        } else {
+            System.out.println("choose an empty place ");
+            System.out.println();
+        }
+    }
+
 }
